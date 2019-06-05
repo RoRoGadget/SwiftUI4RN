4ed5e261a3630fbeaef5a391a9371c22fdb0d0e4
Rohan Panchal <rohan@rohanpanchal.com>
2019-06-05 11:01:26 -0500

    Syntax cleanup for Size and State.

--- Playground.playground/Pages/State.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/State.xcplaygroundpage/Contents.swift
@@ -9,14 +9,24 @@ struct Blink: View {
     @State var isShowingText: Bool = true
     
     var body: some View {
-        VStack {
+        Group {
             Spacer()
             Toggle(isOn: $isShowingText) {
                 Text("Show Text")
             }
             Spacer()
+            
             (self.isShowingText)
-                ? Text("Hello World!")
+                ? Text("I love to toggle")
+                : nil
+            (self.isShowingText)
+                ? Text("Yes blinking is so great")
+                : nil
+            (self.isShowingText)
+                ? Text("Why did they ever take this out of Swift")
+                : nil
+            (self.isShowingText)
+                ? Text("Look at me look at me look at me")
                 : nil
             Spacer()
         }
